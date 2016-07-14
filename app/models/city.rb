26a8fcb5c29a2026a45bb4a0be069cdd0f34548a
Stefan <palmerstefan@yahoo.com>
2016-07-13 18:25:46 -0600

    Added validations
    [#143545678]

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,7 @@
 class City < ApplicationRecord
+  validates :elevation, numericality: {
+    greater_than_or_equal_to: 3315,
+    if: ->(city) {city.state == 'CO'},
+    message: 'is too low, the lowest point in CO is 3315 feet'
+  }
 end
